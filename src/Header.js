50ac81f4aa4f9b0b34cce4eb0109e5c87110a75e
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-19 18:00:09 -0400

    updated headers added tagline

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Header = props => <div><h1>SickSims</h1></div>
+const Header = props => <div><h1>SickSims</h1><h3><i>If 2020 was a game, this would be it.</i></h3><br></br></div>
 
 export default Header
