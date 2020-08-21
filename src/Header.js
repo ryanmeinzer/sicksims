44059dfeedd551a89c0d57d1a8c2e82c50dc82f0
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-21 13:05:20 -0400

    updated instructions

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Header = props => <div><h1>SickSims</h1><h4><i>Save the world!</i></h4><h6>Click naive people 🥴 to make them safe 😷 before they get sick 🤢. <br></br> Click sick people to send them home to recover back.</h6></div>
+const Header = props => <div><h1>SickSims</h1><h4><i>Save the world!</i></h4><h6>Click naive people 🥴 to make them safe 😷 before they get sick 🤢. <br></br> Click sick people 🤢 to send them home to recover back to naive 🥴.</h6></div>
 
 export default Header
