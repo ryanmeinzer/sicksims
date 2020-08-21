8de8b951f79919cdbd6676a5274232f8227a7bec
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-21 00:54:37 -0400

    updated server and procfile for heroku deploy

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
-const path = require('path');
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+// const publicPath = path.join(__dirname, '..', 'public');
+// const port = process.env.PORT || 3000;
+// app.use(express.static(publicPath));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(publicPath, 'index.html'));
+// });
+// app.listen(port, () => {
+//     console.log('Server is up!');
+// });
+
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
-app.listen(port, () => {
-    console.log('Server is up!');
-});
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 3000);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join('build', 'index.html'));
+    });
+}
