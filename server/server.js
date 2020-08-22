f7993d0d99f99afd7df1106efe92a683a98d5389
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-21 22:42:54 -0400

    added require path to server.js for heroku

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@
 //     console.log('Server is up!');
 // });
 
+const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/'));
