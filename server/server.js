4f940436f476c3a9da897060664f2c99b07f3aed
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-22 13:56:38 -0400

    take fourteen server.js

--- server/server.js
+++ server/server.js
@@ -147,16 +147,30 @@
 // app.listen(process.env.PORT || 3000);
 
 // Take Thirteen:
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+// const publicPath = path.join(__dirname, 'public', '/');
+
+// app.use(express.static(__dirname + '/'));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(__dirname + '/'));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join('publicPath', 'index.html'));
+//     });
+// }
+// app.listen(process.env.PORT || 3000);
+
+// Take Fourteen:
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, 'public', '/');
 
 app.use(express.static(__dirname + '/'));
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname + '/'));
+    app.use(express.static('build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join('publicPath', 'index.html'));
+        res.sendFile(path.join('build', 'index.html'));
     });
 }
 app.listen(process.env.PORT || 3000);
