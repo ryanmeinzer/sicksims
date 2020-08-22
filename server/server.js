b178339c1c2cde14b8f71e5d1ba6e06f16550fd0
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-21 23:13:06 -0400

    take five for server.js for heroku

--- server/server.js
+++ server/server.js
@@ -11,19 +11,19 @@
 //     console.log('Server is up!');
 // });
 
-// Take Two:
+// Take Two (works but only without react routes):
 // const path = require('path');
 // const express = require('express');
 // const app = express();
-// app.use(express.static(__dirname + '/'));
-// app.listen(process.env.PORT || 3000);
 
+// app.use(express.static(__dirname + '/'));
 // if (process.env.NODE_ENV === 'production') {
 //     app.use(express.static('build'));
 //     app.get('*', (req, res) => {
 //         res.sendFile(path.join('build', 'index.html'));
 //     });
 // }
+// app.listen(process.env.PORT || 3000);
 
 // Take Three:
 // const path = require('path');
@@ -37,12 +37,24 @@
 // app.listen(process.env.PORT || 3000);
 
 // Take Four:
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+
+// app.use(express.static('client/build'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
+// app.listen(process.env.PORT || 3000);
+
+// Take Five:
 const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static('client/build'));
+app.use(express.static('build'));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join('build', 'index.html'));
 });
+
 app.listen(process.env.PORT || 3000);
