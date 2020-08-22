297c34f5b0fa56df774108a7b1b90116d0f164ab
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-21 23:07:11 -0400

    updated server take four for heroku

--- server/server.js
+++ server/server.js
@@ -26,12 +26,23 @@
 // }
 
 // Take Three:
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+
+// app.use(express.static(__dirname + '/'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'index.html'));
+// });
+// app.listen(process.env.PORT || 3000);
+
+// Take Four:
 const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static('client/build'));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 app.listen(process.env.PORT || 3000);
