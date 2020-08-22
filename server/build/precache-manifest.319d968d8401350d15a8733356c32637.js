2ec09fb9e5a7dacce03a0044f86a3d3d7df65777
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-21 23:58:10 -0400

    re-ran npm run build and moved entire build directory to server...for heroku

---
+++ server/build/precache-manifest.319d968d8401350d15a8733356c32637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3b322927f58ac612ae0183a5f3a0b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdd31ee0b1746e1d87b2",
+    "url": "/static/css/main.3dfed0c4.chunk.css"
+  },
+  {
+    "revision": "9fd5c3e64edd26fdd017",
+    "url": "/static/js/2.e3556afd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e3556afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd31ee0b1746e1d87b2",
+    "url": "/static/js/main.f212fa27.chunk.js"
+  },
+  {
+    "revision": "2928eed3df6b23a329ce",
+    "url": "/static/js/runtime-main.53cdd7e4.js"
+  }
+]);
