d42c9b08e5b2803881ca3770dc5d9dd23e550236
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-22 14:03:41 -0400

    take fourteen server.js

--- server/server.js
+++ server/server.js
@@ -170,7 +170,7 @@ app.use(express.static(__dirname + '/'));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join('build', 'index.html'));
+        res.sendFile(__dirname + '/index.html');
     });
 }
 app.listen(process.env.PORT || 3000);
