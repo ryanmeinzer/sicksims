8f0fb2015af9e02b194dbd9889119ced6ecb54d7
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-30 13:51:18 -0400

    implemented delay on finished game alert to convert people to saved prior

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
     if (!this.props.people.find(({ status }) => status === 'naive' || status === 'sick' || status === 'quarantined')) {
       this.props.dispatchedSafeToSavedChanger()
       let score = parseInt(this.props.people.filter(({ status }) => status === 'saved').length * 10)
-      alert(`Congrats - you saved (some of) the world! Your score is ${score}`)
+      setTimeout(() => alert(`Congrats - you saved (some of) the world! Your score is ${score}`), 1000)
       this.componentWillUnmount()
     }
   }
