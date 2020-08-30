d5ded901ddf4b9a891c039bd9613dc4a963b12b8
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-30 13:01:42 -0400

    moved all css to index.css and deleted app.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import './App.css'
 import Header from './Header.js'
 import NaivePerson from './NaivePerson.js'
 import QuarantinedPerson from './QuarantinedPerson.js'
