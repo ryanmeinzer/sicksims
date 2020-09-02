939ec9be09cc2bdf8a3eba57d9075918fe12679f
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-09-02 10:37:19 -0400

    Emphasized click and drag

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ const Header = props =>
     <div className='logo' align='center'>
         <img src={logo} alt="SickSims Logo" className="logo" background-color="transparent"></img>
     </div>
-        <h6>Click naive people <span role="img" aria-label="naive person emoji">🥴</span> to make them safe <span role="img" aria-label="safe person emoji">😷</span> before they become sick <span role="img" aria-label="sick person emoji">🤢</span> <br></br> Drag sick people <span role="img" aria-label="sick person emoji">🤢</span> into quarantine for recovery <span role="img" aria-label="quarantined person emoji">🤒</span> before they become terminal <span role="img" aria-label="terminal person emoji">😵</span></h6>
+        <h6><span className='emphasizedText'>Click</span> naive people<span role="img" aria-label="naive person emoji">🥴</span> to make them safe <span role="img" aria-label="safe person emoji">😷</span> before they become sick <span role="img" aria-label="sick person emoji">🤢</span> <br></br> <span className='emphasizedText'>Drag & Drop</span> sick people <span role="img" aria-label="sick person emoji">🤢</span> into quarantine for recovery <span role="img" aria-label="quarantined person emoji">🤒</span> before they become terminal <span role="img" aria-label="terminal person emoji">😵</span></h6>
     <p><Link to="/superheros"><button className='superherosButton'><i>See Top Superheros</i></button></Link>
             <button className='playButton' onClick={props.startGameButton}><i>Start Game - Save the world!</i> <span role="img" aria-label="saved person emoji">🥰</span></button>
     <Link to="/score"><button className='scoreButton'><i>Save Your Score</i></button></Link></p>
