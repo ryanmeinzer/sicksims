8fc1894dc1d8cf8de418e98de04fa22e1178d957
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-09-02 23:49:45 -0400

    rendered modified game directions based on desktop or mobile view

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,11 @@ const Header = () =>
         <img src={logo} alt="SickSims Logo" className="logo" background-color="transparent"></img>
     </div>
     <div className='gameDirections'>
-        <h6><span className='emphasizedText'>Click</span> naive people<span role="img" aria-label="naive person emoji">🥴</span> to make them safe <span role="img" aria-label="safe person emoji">😷</span> before they become sick <span role="img" aria-label="sick person emoji">🤢</span> <br></br> <span className='emphasizedText'>Drag & Drop</span> sick people <span role="img" aria-label="sick person emoji">🤢</span> into quarantine for recovery <span role="img" aria-label="quarantined person emoji">🤒</span> before they become terminal <span role="img" aria-label="terminal person emoji">😵</span></h6>
+
+        <h6 className='desktop-only'><span className='emphasizedText'>Click</span> naive people<span role="img" aria-label="naive person emoji">🥴</span> to make them safe <span role="img" aria-label="safe person emoji">😷</span> before they become sick <span role="img" aria-label="sick person emoji">🤢</span> <br></br> <span className='emphasizedText'>Drag & Drop</span> sick people <span role="img" aria-label="sick person emoji">🤢</span> into quarantine for recovery <span role="img" aria-label="quarantined person emoji">🤒</span> before they become terminal <span role="img" aria-label="terminal person emoji">😵</span></h6>
+
+        <h6 className='mobile-only'><span className='emphasizedText'>Click</span> naive people<span role="img" aria-label="naive person emoji">🥴</span> to make them safe <span role="img" aria-label="safe person emoji">😷</span> before they become sick <span role="img" aria-label="sick person emoji">🤢</span> <br></br> <span className='emphasizedText'>Click</span> sick people <span role="img" aria-label="sick person emoji">🤢</span> to make them quarantined for recovery <span role="img" aria-label="quarantined person emoji">🤒</span> before they become terminal <span role="img" aria-label="terminal person emoji">😵</span></h6>
+
     </div>
 </div>
 
