3ace79717af3688c0e755bc55c159be3b1d85b5d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-09-03 22:37:33 -0400

    re-updated direct to top of page after game finish

--- src/App.js
+++ src/App.js
@@ -41,8 +41,8 @@ class App extends Component {
       this.props.dispatchedSafeToSavedChanger()
       let score = parseInt(this.props.people.filter(({ status }) => status === 'saved').length * 10)
       setTimeout(() => alert(`Congrats - you saved (some of) the world! Your score is ${score}`), 1000)
-      window.location.replace('/play#Header')
       this.componentWillUnmount()
+      window.location.replace('/play#Header')
     }
   }
 
