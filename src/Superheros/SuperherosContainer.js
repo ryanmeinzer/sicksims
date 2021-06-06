5fa821c7adeb31d8cd01ea957b7d72a766a6df3a
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-06-05 23:04:13 -0700

    converted connect to useDispatch and useSelector hooks in Superheros

--- src/Superheros/SuperherosContainer.js
+++ src/Superheros/SuperherosContainer.js
@@ -1,76 +1,61 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchSuperheros } from '../redux/actions'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {fetchSuperheros} from '../redux/actions'
 import Superheros from './Superheros.js'
 import { fetchStats } from '../redux/actions'
 import RePlayButton from '../RePlayButton.js'
 
-class SuperherosContainer extends Component {
+const SuperherosContainer = (interval) => {
 
-    // fetch superheros upon app load
-    componentDidMount() {
-        this.props.dispatchedFetchSuperheros()
-        this.props.dispatchedFetchStats()
-    }
+    const mappedSuperheros = useSelector(state => state.superheros)
+    const loading = useSelector(state => state.loading)
+    const mappedStats = useSelector(state => state.stats)
+    const dispatch = useDispatch()
 
-    componentWillUnmount() {
-        clearInterval(this.interval)
-    }
+    useEffect(() => {
+        dispatch(fetchSuperheros())
+        dispatch(fetchStats())
+        return () => {
+            clearInterval(interval)
+        }
+    }, [dispatch, interval])
 
     // handle fetch of superheros upon app load with loading message if still loading
-    handleLoading = () => {
-        // console.log(this.props.loading)
-        if (this.props.loading) {
+    const handleLoading = () => {
+        if (loading) {
             return <div>Loading...</div>
         } else {
-            return <Superheros mappedSuperheros={this.props.mappedSuperheros} />
+            return <Superheros mappedSuperheros={mappedSuperheros} />
         }
     }
 
-    render() {
-        return (
-            <>
-            < RePlayButton />
-            <div className="SuperherosPSAContainer">
-                <div className="SuperherosContainer">
-                    <h4>Top Superheros:</h4>
-                    {this.handleLoading()}
-                </div>
-                <div className="SuperherosPSAOverlay">
-                    <div className="SuperherosPSAOverlayText">
-                        <p>
-                            <span className='PointUpEmoji' role='img' aria-label='point up emoji'>☝️</span> Restart Game Above <span className='PointUpEmoji' role='img' aria-label='point up emoji'>☝️</span>
-                        </p>
-                        <p>
-                            Unfortunately, this is just a game to create awareness and public safety. COVID-19 is real.
-                        </p>
-                        <p>
-                            In fact, in the last 24 hours there have been <b>{parseInt(this.props.mappedStats.NewConfirmed).toLocaleString()}</b> new cases and <b>{parseInt(this.props.mappedStats.NewDeaths).toLocaleString()}</b> new deaths of COVID-19.
-                        </p>
-                        <p align='center'>
-                            <i>Please wear masks and stay home in the hopes to get better if you are not feeling well!</i>
-                        </p>
-                    </div>
+    return (
+        <>
+        < RePlayButton />
+        <div className="SuperherosPSAContainer">
+            <div className="SuperherosContainer">
+                <h4>Top Superheros:</h4>
+                {handleLoading()}
+            </div>
+            <div className="SuperherosPSAOverlay">
+                <div className="SuperherosPSAOverlayText">
+                    <p>
+                        <span className='PointUpEmoji' role='img' aria-label='point up emoji'>☝️</span> Restart Game Above <span className='PointUpEmoji' role='img' aria-label='point up emoji'>☝️</span>
+                    </p>
+                    <p>
+                        Unfortunately, this is just a game to create awareness and public safety. COVID-19 is real.
+                    </p>
+                    <p>
+                        In fact, in the last 24 hours there have been <b>{parseInt(mappedStats.NewConfirmed).toLocaleString()}</b> new cases and <b>{parseInt(mappedStats.NewDeaths).toLocaleString()}</b> new deaths of COVID-19.
+                    </p>
+                    <p align='center'>
+                        <i>Please wear masks and stay home in the hopes to get better if you are not feeling well!</i>
+                    </p>
                 </div>
             </div>
-            </>
-        )
-    }
+        </div>
+        </>
+    )
 }
 
-// can't just use destructured people anymore with superheros fetch
-// const mapStateToProps = ({ people }) => ({ people })
-const mapStateToProps = state => {
-    return {
-        mappedSuperheros: state.superheros,
-        loading: state.loading, 
-        mappedStats: state.stats
-    }
-}
-
-const mapDispatchToProps = dispatch => ({
-    dispatchedFetchSuperheros: () => dispatch(fetchSuperheros()), 
-    dispatchedFetchStats: () => dispatch(fetchStats())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(SuperherosContainer)
+export default SuperherosContainer
