f8408ec6f82b3c2babdd89138f30e86429b0d2a0
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-06-12 18:20:47 -0700

    converted connect, state and lifecycle methods to hooks in QuarantinedPerson

--- src/QuarantinedPerson.js
+++ src/QuarantinedPerson.js
@@ -1,58 +1,43 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, {useEffect, useState} from 'react'
+import {useDispatch} from 'react-redux'
 import { quarantinedToNaiveChanger } from './redux/actions'
 import UIfx from 'uifx'
 import sound from './sounds/naive.mp3'
 
 const naiveSound = new UIfx(sound)
 
-class QuarantinedPerson extends Component {
+const QuarantinedPerson = (props) => {
 
-    state = {
-        time: 8
-    }
+    const [time, setTime] = useState(8)
+    const dispatch = useDispatch()
 
-    componentDidMount = () => {
-        this.startInterval()
-    }
+    let interval
 
-    componentWillUnmount() {
-        clearInterval(this.interval)
-    }
-
-    startInterval = () => {
-        this.interval = setInterval(this.updateCounter, 1000)
-    }
-
-    checkTime = () => {
-        // console.log("checking time", this.state.time, this.props.id)
-    if (this.state.time <= 0) {
-        // console.log("woo!")
-        this.props.dispatchedQuarantinedToNaiveChanger(this.props.id)
-        // console.log("quarantined to risky changed", this.props.id)
-        naiveSound.play(0.05)
+    useEffect(() => {
+        interval = setInterval(updateCounter, 1000)
+        return () => {
+            clearInterval(interval)
         }
+    }, [])
+
+    useEffect(() => {
+        if (time <= 0) {
+            dispatch(quarantinedToNaiveChanger(props.id))
+            naiveSound.play(0.05)
+        }
+    }, [time])
+
+    const updateCounter = () => {
+        setTime(prevState => prevState - 1)
     }
 
-    updateCounter = () => {
-        this.setState((prevState) => ({time: prevState.time - 1 }), this.checkTime)
-        // console.log(this.props.id)
-    }
-
-    render() {
-        return(
-            <>
-                <span className='quarantinedPersonEmoji' role='img' aria-label='quarantined person emoji' id={this.props.id} style={{ cursor: 'not-allowed' }}>🤒</span>
-                <span className='quarantinedPersonCountdown'> ▽ {this.state.time}</span>
-            </>
-        )
-    }
+    return(
+        <>
+            <span className='quarantinedPersonEmoji' role='img' aria-label='quarantined person emoji' id={props.id} style={{ cursor: 'not-allowed' }}>🤒</span>
+            <span className='quarantinedPersonCountdown'> ▽ {time}</span>
+        </>
+    )
 
 }
 
-const mapDispatchToProps = dispatch => ({
-    // dispatchedMakeSafe: (e) => dispatch(makeSafe(e)),
-    dispatchedQuarantinedToNaiveChanger: (quarantinedPersonId) => dispatch(quarantinedToNaiveChanger(quarantinedPersonId))
-})
-
-export default connect(null, mapDispatchToProps)(QuarantinedPerson)
+export default QuarantinedPerson
