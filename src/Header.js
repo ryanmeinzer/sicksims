3967abd4ef7894ea83e6e0dc7e8a5d69413ea152
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-06-12 18:29:30 -0700

    converted connect to hooks and from class to functional for Header and Navigation components

--- src/Header.js
+++ src/Header.js
@@ -1,36 +1,31 @@
-import React, { Component } from 'react'
+import React, {useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
 import Logo from './Logo.js'
-import { connect } from 'react-redux'
 
-class Header extends Component {
-    render() {
-        let gameComplete = this.props.people.find(person => person.status === 'saved')
+const Header = () => {
 
-        if (gameComplete || window.location.pathname === '/score' || window.location.pathname === '/superheros' || window.location.pathname === '/stats') {
-            return (
+    const people = useSelector(state => state.people)
+
+    let gameComplete = people.find(person => person.status === 'saved')
+
+    if (gameComplete || window.location.pathname === '/score' || window.location.pathname === '/superheros' || window.location.pathname === '/stats') {
+        return (
+            < Logo />
+        )
+    } else {
+        return (
+            <>
                 < Logo />
-            )
-        } else {
-            return (
-                <>
-                    < Logo />
-                    <div className='Header'>
-                        <div className='gameDirections'>
-                            <h6 className='desktop-only'><span className='emphasizedText'>Click</span> naive people <span role="img" aria-label="naive person emoji">🥴</span> to make them safe <span role="img" aria-label="safe person emoji">😷</span> before they become sick <span role="img" aria-label="sick person emoji">🤢</span> <br></br> <span className='emphasizedText'>Drag & Drop</span> sick people <span role="img" aria-label="sick person emoji">🤢</span> into quarantine for recovery <span role="img" aria-label="quarantined person emoji">🤒</span> before they become terminal <span role="img" aria-label="terminal person emoji">😵</span></h6>
+                <div className='Header'>
+                    <div className='gameDirections'>
+                        <h6 className='desktop-only'><span className='emphasizedText'>Click</span> naive people <span role="img" aria-label="naive person emoji">🥴</span> to make them safe <span role="img" aria-label="safe person emoji">😷</span> before they become sick <span role="img" aria-label="sick person emoji">🤢</span> <br></br> <span className='emphasizedText'>Drag & Drop</span> sick people <span role="img" aria-label="sick person emoji">🤢</span> into quarantine for recovery <span role="img" aria-label="quarantined person emoji">🤒</span> before they become terminal <span role="img" aria-label="terminal person emoji">😵</span></h6>
 
-                            <h6 className='mobile-only'><span className='emphasizedText'>Tap</span> naive people <span role="img" aria-label="naive person emoji">🥴</span> to make them safe <span role="img" aria-label="safe person emoji">😷</span> before they become sick <span role="img" aria-label="sick person emoji">🤢</span>. <br></br> <span className='emphasizedText'>Tap</span> sick people <span role="img" aria-label="sick person emoji">🤢</span> to quarantine them for recovery <span role="img" aria-label="quarantined person emoji">🤒</span> before they become terminal <span role="img" aria-label="terminal person emoji">😵</span>.</h6>
-                        </div>
+                        <h6 className='mobile-only'><span className='emphasizedText'>Tap</span> naive people <span role="img" aria-label="naive person emoji">🥴</span> to make them safe <span role="img" aria-label="safe person emoji">😷</span> before they become sick <span role="img" aria-label="sick person emoji">🤢</span>. <br></br> <span className='emphasizedText'>Tap</span> sick people <span role="img" aria-label="sick person emoji">🤢</span> to quarantine them for recovery <span role="img" aria-label="quarantined person emoji">🤒</span> before they become terminal <span role="img" aria-label="terminal person emoji">😵</span>.</h6>
                     </div>
-                </>
-            )
-        }
+                </div>
+            </>
+        )
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        people: state.people
-    }
-}
-
-export default connect(mapStateToProps)(Header)
+export default Header
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Navigation extends Component {
+const Navigation = () => {
 
     // hard refresh to reset state for game restart
-    startGame = () => {
+    const startGame = () => {
         window.location.replace('/play#InPublicContainer')
     }
 
-    render() {
-        return (
-            <div className='gameIncompleteNavigation'>
-                <button className='playButton' onClick={this.startGame}><i>Start Game - Save the world!</i> <span role="img" aria-label="saved person emoji">🥰</span></button>
-            </div>
-        )
-    }
+    return (
+        <div className='gameIncompleteNavigation'>
+            <button className='playButton' onClick={startGame}><i>Start Game - Save the world!</i> <span role="img" aria-label="saved person emoji">🥰</span></button>
+        </div>
+    )
+    
 }
 
 export default Navigation
